23d153bf63b90479e92e802620954e09a4a0240d
Mateusz Hercun <mateusz.hercun@gmail.com>
2017-10-15 21:08:21 +0200

    Knn now runs in parallel but needs fix (accuracy is lower somehow).

--- src/classifier/Knn.hpp
+++ src/classifier/Knn.hpp
@@ -1,22 +1,38 @@
-#ifndef KNN_HPP_
-#define KNN_HPP_
+#ifndef CLASSIFIER_KNN_HPP_
+#define CLASSIFIER_KNN_HPP_
 
 #include <algorithm>
+#include <atomic>
 #include <cmath>
 #include <iostream>
 #include <map>
 #include <memory>
+#include <mutex>
 #include <string>
+#include <thread>
 #include <utility>
 #include <vector>
 
-#include "../data/Iris.hpp" // remove relative path - fix cmake
+#include <boost/chrono/process_cpu_clocks.hpp>
+#include <boost/optional.hpp>
+
+#include "src/data/Iris.hpp"
+
+/*#if(ENABLE_OPENMP == 1)
+    #include <omp.h>
+    #pragma message("Compile with OpenMP")
+#else
+    #define OMP_NUM_THREADS 0
+    #pragma message("Compile without OpenMP")
+#endif*/
 
 template <typename DataType>
 using NeighborWithDistance = typename std::pair<DataType, float>;
 
 template <typename DataType>
 using NeighborsWithDistances = typename std::vector<std::pair<DataType, float> >;
+
+using LockGuard = std::lock_guard<std::mutex>;
 
 namespace
 {
@@ -41,12 +57,12 @@ template <typename DataType>
 NeighborsWithDistances<DataType> getAllNeighbors(const DataType& test,
     const std::vector<DataType>& trainSet)
 {
-    //std::cout << "in getAllNeighbors(...)" << std::endl;
+    // std::cout << "in getAllNeighbors(...)" << std::endl;
     NeighborsWithDistances<DataType> neighborsWithDistances;
 
     for (auto& train : trainSet)
     {
-        //std::cout << "train: " << train << std::endl;
+        // std::cout << "train: " << train << std::endl;
         neighborsWithDistances.push_back(std::make_pair(
             train, countEuclideanDistance(test, train)));
     }
@@ -58,7 +74,7 @@ template <typename DataType>
 NeighborsWithDistances<DataType> getNearestNeighbors(NeighborsWithDistances<DataType> allNeighbors,
     const unsigned int k)
 {
-    //std::cout << "in getNearestNeighbors(...)" << std::endl;
+    // std::cout << "in getNearestNeighbors(...)" << std::endl;
 
     std::sort(allNeighbors.begin(), allNeighbors.end(),
         [](const auto& lhs, const auto& rhs)
@@ -75,7 +91,7 @@ void resetClassNameToAmountMap(const DataType& test,
 {
     // get possible classes from data interface, not from object
 
-    //std::cout << "in resetClassNameToAmountMap(...)" << std::endl;
+    // std::cout << "in resetClassNameToAmountMap(...)" << std::endl;
 
     if (classNameToAmountMap.empty())
     {
@@ -96,7 +112,7 @@ template <typename DataType>
 std::string chooseClassBasedOnNeighbors(const NeighborsWithDistances<DataType>& kNearestNeighbors,
     std::map<std::string, int>& classNameToAmountMap)
 {
-    //std::cout << "in chooseClassBasedOnNeighbors(...)" << std::endl;
+    // std::cout << "in chooseClassBasedOnNeighbors(...)" << std::endl;
 
     for (const auto& kNearestNeighbor : kNearestNeighbors)
     {
@@ -113,7 +129,7 @@ std::string chooseClassBasedOnNeighbors(const NeighborsWithDistances<DataType>& 
 
     auto dominantClassNameToAmount = std::max_element(classNameToAmountMap.begin(),
         classNameToAmountMap.end(),
-        [](const std::pair<std::string, int>& lhs, const std::pair<std::string, int>& rhs)
+        [](const auto& lhs, const auto& rhs)
         {
             return lhs.second < rhs.second;
         });
@@ -131,45 +147,83 @@ class Knn
 public:
     template <typename DataType>
     void predict(const std::vector<DataType>& testSet, const std::vector<DataType>& trainSet,
-        const unsigned int k)
+        const unsigned int k, const boost::optional<unsigned int> numOfThreads)
     {
-        //std::cout << "in predict(...)" << std::endl;
-
         std::map<std::string, int> classNameToAmountMap;
-        // All of these are made float to make accuracy easier to compute
-        float correctChoices = 0.0f;
-        float incorrectChoices = 0.0f;
-        float allChoices = static_cast<float>(testSet.size());  // it will be removed in refactor
+        std::atomic<int> correctChoices {0};
+        float allChoices = static_cast<float>(testSet.size());
 
-        for (const auto& test : testSet)
+        if (!numOfThreads)
         {
-            resetClassNameToAmountMap(test, classNameToAmountMap);
-            auto kNearestNeighbors = getNearestNeighbors(getAllNeighbors(test, trainSet), k);
-
-            std::string dominantClassInNeighborhood = chooseClassBasedOnNeighbors(kNearestNeighbors,
-                classNameToAmountMap);
-
-            if (dominantClassInNeighborhood == test.getClassName())
-            {
-                ++correctChoices;
-            }
-
-            else
-            {
-                ++incorrectChoices;
-            }
+            makePredictions(testSet, trainSet, k, classNameToAmountMap, correctChoices,
+                boost::none, boost::none);
+            accuracy_ = (2*correctChoices - allChoices) / allChoices;
+            return;
         }
-        std::cout << "correctChoices: " << correctChoices << std::endl;
-        std::cout << "incorrectChoices: " << incorrectChoices << std::endl;
-        std::cout << "allChoices: " << allChoices << std::endl;
-        accuracy_ = (correctChoices - incorrectChoices) / allChoices;
-    };
+
+        for (unsigned int numOfThread = 0; numOfThread < *numOfThreads; ++numOfThread)
+        {
+            workingThreads_.emplace_back(
+                [&, numOfThread]
+                {
+                    this->makePredictions(testSet, trainSet, k, classNameToAmountMap,
+                        correctChoices, numOfThread, numOfThreads);
+                });
+        }
+
+        for (auto& thread : workingThreads_)
+        {
+            thread.join();
+        }
+
+        workingThreads_.clear();
+
+        accuracy_ = correctChoices / allChoices;
+
+        // auto stop = boost::chrono::process_user_cpu_clock::now();
+    }
 
     float getAccuracy() const {return accuracy_;}
+    // unsigned int getMeasuredTime() const {return measuredTime_;}
 
 private:
+    template <typename DataType>
+    void makePredictions(const std::vector<DataType>& testSet,
+        const std::vector<DataType>& trainSet, const unsigned int& k,
+        std::map<std::string, int>& classNameToAmountMap, std::atomic<int>& correctChoices,
+        boost::optional<unsigned int> numOfThread, boost::optional<unsigned int> numOfThreads)
+    {
+        LockGuard guard(predictionMutex_);
+
+        auto begin = testSet.cbegin();
+        auto end = testSet.cend();
+
+        if (numOfThread && numOfThreads)
+        {
+            begin = testSet.cbegin() + *numOfThread * (testSet.size() / *numOfThreads);
+            end = begin + (testSet.size() / *numOfThreads) - 1;
+        }
+
+        for (auto it = begin; it != end; it++)
+        {
+            resetClassNameToAmountMap(*it, classNameToAmountMap);
+            auto kNearestNeighbors = getNearestNeighbors(getAllNeighbors(*it, trainSet), k);
+
+            std::string dominantClassInNeighborhood = chooseClassBasedOnNeighbors(
+                kNearestNeighbors, classNameToAmountMap);
+
+            if (dominantClassInNeighborhood == it->getClassName())
+            {
+                correctChoices++;
+            }
+        }
+    }
+
     float accuracy_ {0.0f};
+    std::mutex predictionMutex_ {};
+    // unsigned int measuredTime_{0}; // time will be measured outside
+    std::vector<std::thread> workingThreads_;
 };
 
 }  // namespace classifier
-#endif  // KNN_HPP_
+#endif  // CLASSIFIER_KNN_HPP_
--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,64 @@
+#include <iostream>
+#include <thread>
+
+#include <boost/optional.hpp>
+#include <boost/range/irange.hpp>
+
 #include "classifier/Knn.hpp"
 #include "data/DataHandler.hpp"
 #include "data/Iris.hpp"
 
 int main()
 {
+    const unsigned int k = 5;
+    const float split = 0.7;
+    float accSum {0.0f};
+    int runs = 100;
+    // unsigned int numOfThreads = std::thread::hardware_concurrency();
+
     data::DataHandler<data::Iris> irisDataHandler;
     irisDataHandler.loadData("../iris.data");
-    irisDataHandler.splitData(0.7);
-
+    irisDataHandler.splitData(split);
     classifier::Knn kNN;
-    kNN.predict<data::Iris>(irisDataHandler.getTestSet(), irisDataHandler.getTrainSet(), 5);
-    std::cout << "accuracy: " << kNN.getAccuracy() << std::endl;
+    /*kNN.predict(irisDataHandler.getTestSet(), irisDataHandler.getTrainSet(), k, 2);
+    std::cout << "accuracy: " << kNN.getAccuracy() << std::endl;*/
+
+    std::cout << "Running non-threaded kNN" << std::endl;
+
+    for (auto run : boost::irange(0, runs))
+    {
+        irisDataHandler.splitData(split);
+        std::cout << "run: " << run << std::endl;
+        kNN.predict(irisDataHandler.getTestSet(), irisDataHandler.getTrainSet(), k, boost::none);
+        // const auto accuracy = kNN.getAccuracy();
+        // std::cout << "accuracy: " << accuracy << std::endl;
+        accSum += kNN.getAccuracy();
+    }
+
+    std::cout << "Mean accuracy for " << runs << " runs of kNN (k=" << k << ", split=" << split
+        << "): " << accSum/runs << std::endl;
+    // std::cout << "measured time: " << kNN.getMeasuredTime() << "[nanoseconds]" << std::endl;
+
+    accSum = 0.0f;
+
+    std::cout << "Number of hardware thread contexts: " << std::thread::hardware_concurrency()
+        << std::endl;
+
+    std::cout << "Running threaded kNN" << std::endl;
+
+    for (auto run : boost::irange(0, runs))
+    {
+        irisDataHandler.splitData(split);
+        std::cout << "run: " << run << std::endl;
+        kNN.predict(irisDataHandler.getTestSet(), irisDataHandler.getTrainSet(), k,
+            std::thread::hardware_concurrency()/2);
+        // const auto accuracy = kNN.getAccuracy();
+        // std::cout << "accuracy: " << accuracy << std::endl;
+        accSum += kNN.getAccuracy();
+    }
+
+    std::cout << "Mean accuracy for " << runs << " runs of threaded kNN (k=" << k << ", split="
+        << split << "): " << accSum/runs << std::endl;
 
     return 0;
 }
